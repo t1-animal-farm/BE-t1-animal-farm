058efdd553e8d7d1a8319241b1d8b4a8fb90896d
baeksbs3 <baeksbs3@gmail.com>
2022-12-16 20:16:23 +0900

    User model, users table created

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
-
+const Post = require('./post');
+const User = require('./user');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
@@ -12,13 +13,15 @@ const sequelize = new Sequelize(
 );
 
 db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
 
 db.Post = Post;
+db.User = User;
 
 // 여기에다가 db객체에 모델들을 넣어주세요.
 
 Post.init(sequelize);
-
+User.init(sequelize);
 // 여기다가 각 모델의 init함수에 sequelize객체를 연결해주세요.
 
 module.exports = db;
---
+++ models/user.js
@@ -0,0 +1,43 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        userId: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        email: {
+          unique: true,
+          type: Sequelize.STRING(45),
+        },
+        nickname: {
+          unique: true,
+          allowNull: false,
+          type: Sequelize.STRING(45),
+        },
+        password: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        modelName: 'User',
+        tableName: 'users',
+        paranoid: false,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      }
+    );
+  }
+  static associate(db) {
+    this.hasMany(db.Post, { foreignKey: 'userId' });
+    this.hasMany(db.Comment, { foreignKey: 'userId' });
+    this.hasMany(db.Image, { foreignKey: 'userId' });
+  }
+};
