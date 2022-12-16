061907dc320758d4f2c0524f49e5028a1fdda798
baeksbs3 <baeksbs3@gmail.com>
2022-12-16 17:22:54 +0900

    file created

--- models/index.js
+++ models/index.js
@@ -1,20 +1,24 @@
 const Sequelize = require('sequelize');
 
-
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 db.sequelize = sequelize;
 
 db.Post = Post;
+
 // 여기에다가 db객체에 모델들을 넣어주세요.
 
-
 Post.init(sequelize);
-// 여기다가 각 모델의 init함수에 sequelize객체를 연결해주세요.
 
+// 여기다가 각 모델의 init함수에 sequelize객체를 연결해주세요.
 
 module.exports = db;
