f10680de90c17aedcfccc6963b4efbca83071e1d
baeksbs3 <baeksbs3@gmail.com>
2022-12-16 21:27:56 +0900

    router added on all routes files

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,13 @@ const router = express.Router();
 const signupRouter = require('./signup.routes');
 const loginRouter = require('./login.routes');
 const logoutRouter = require('./logout.routes');
-const logoutRouter = require('./main.routes');
-const logoutRouter = require('./auth.routes');
-const logoutRouter = require('./post.routes');
-const commentRouter = require('./comment.routes');
+const mainRouter = require('./main.routes');
+const authRouter = require('./auth.routes');
 
 router.use('/signup/', signupRouter);
 router.use('/login/', loginRouter);
 router.use('/logout/', logoutRouter);
 router.use('/main/', mainRouter);
 router.use('/auth/', authRouter);
-router.use('/posts/', postRouter);
-router.use('/comments/', commentRouter);
 
-module.exports = indexRouter;
+module.exports = router;
