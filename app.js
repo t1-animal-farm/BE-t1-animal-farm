c6f39fbc1d4679d694ef138fb5083f583f69b418
Park youngho <snu910501@naver.com>
2022-12-16 16:13:47 +0900

    project setting

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require("morgan");
+const dotenv = require('dotenv').config();
+const cookieParser = require('cookie-parser');
+
+const { sequelize } = require('./models');
+// const postRouter = require("./routes/post"); 라우터 불러오는 곳은 여기
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('DB 연결 되었습니다.')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// app.use('/posts', postRouter); 라우터 연결은 이곳
+
+
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중')
+});
---
+++ models/post.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Post extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+      text: {
+        type: Sequelize.STRING(45),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      userId: {
+        type: Sequelize.INTEGER(45),
+        allowNull: false,
+      },
+    },
+      {
+        sequelize,
+        timestamps: false,
+        modelName: 'Post',
+        tableName: 'posts',
+        paranoid: false,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      })
+  }
+  static associate(db) {
+
+  }
+}
