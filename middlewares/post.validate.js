bc1a3a673d869b48064c379be8981bfd3b8f97ac
Park youngho <snu910501@naver.com>
2022-12-21 12:20:05 +0900

    posts validate fixed

--- middlewares/post.validate.js
+++ middlewares/post.validate.js
@@ -1,7 +1,8 @@
 const ErrorMiddleware = require("../middlewares/errorMiddleware");
 
 exports.validatePost = (text) => {
-  if (!text) {
+  console.log(text == undefined)
+  if (text == '' || text == undefined) {
     const errorMiddleware = new ErrorMiddleware(401, '포스트 형식이 일치하지 않습니다.');
     throw errorMiddleware
   }
--- repositories/login.repository.js
+++ repositories/login.repository.js
@@ -1,8 +1,16 @@
 const { User } = require('../models');
+const ErrorMiddleware = require('../middlewares/errorMiddleware');
+
 class LoginRepository {
   login = async (email) => {
-    const validate = await User.findOne({ where: { email } });
-    return validate;
+    try {
+      const validate = await User.findOne({ where: { email } });
+      return validate;
+    } catch (err) {
+      let error = new ErrorMiddleware(500, 'DB에러');
+      throw error;
+    }
+
   };
 }
 
