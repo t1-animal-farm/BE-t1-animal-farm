9a4b1359121b3dfb264a5c7f1b8d7485c550b054
Park youngho <snu910501@naver.com>
2022-12-21 14:05:24 +0900

    h

--- middlewares/post.validate.js
+++ middlewares/post.validate.js
@@ -1,7 +1,7 @@
 const ErrorMiddleware = require("../middlewares/errorMiddleware");
 
 exports.validatePost = (text) => {
-  console.log(text == undefined)
+  console.log('text', text);
   if (text == '' || text == undefined) {
     const errorMiddleware = new ErrorMiddleware(401, '포스트 형식이 일치하지 않습니다.');
     throw errorMiddleware
